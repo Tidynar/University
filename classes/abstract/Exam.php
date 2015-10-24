b922bad2e0da17910b83357200b127dae0b54ec6
Ruslan <afalin3s@gmail.com>
2015-10-24 21:46:19 +0300

    Edited exam - added toString, Exam object must return name of exam

--- classes/abstract/Exam.php
+++ classes/abstract/Exam.php
@@ -6,9 +6,19 @@
  * Class Exam
  */
 class Exam {
+	private $exam_name = 'Default';
 	public $eid = 0;
 
 	function __construct($exam_id) {
 		$this->eid = $exam_id;
+	}
+
+	/**
+	 * Exam object must return name of exam
+	 *
+	 * @return string
+	 */
+	function __toString() {
+		return $this->exam_name;
 	}
 }
